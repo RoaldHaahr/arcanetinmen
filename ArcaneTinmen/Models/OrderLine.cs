7622411ccefec6c15a480fdb51aa127dbbfba143
RoaldHaahr <roald_832@hotmail.com>
2016-12-04 18:20:51 +0100

    Orderline annotations

--- ArcaneTinmen/Models/OrderLine.cs
+++ ArcaneTinmen/Models/OrderLine.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ArcaneTinmen.Models
 {
     public class OrderLine
     {
+        [Key]
         public int OrderLineId { get; set; }
+        [Required]
         public int OrderPlacementId { get; set; }
+        [Required]
         public int SleeveId { get; set; }
+        [Required]
         public int Quantity { get; set; }
 
         public OrderPlacement OrderPlacement { get; set; }
