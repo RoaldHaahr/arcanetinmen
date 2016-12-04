b13f41fe9519d3ff07f93996620c83b0fce1e702
RoaldHaahr <roald_832@hotmail.com>
2016-12-04 18:31:38 +0100

    Sleeve.cs annotations

--- ArcaneTinmen/Models/Sleeve.cs
+++ ArcaneTinmen/Models/Sleeve.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ArcaneTinmen.Models
 {
     public class Sleeve
     {
+        [Key]
         public int SleeveId { get; set; }
         public string Description { get; set; }
+        [Required(ErrorMessage = "Please enter a height in millimeters")]
         public int Height { get; set; }
+        [Required(ErrorMessage = "Please enter a width in millimeters")]
         public int Width { get; set; }
         // Why is there both a cost- and a saleprice?
         public decimal CostPrice { get; set; }
+        [Required(ErrorMessage = "Please enter the price for the sleeve")]
         public decimal SalePrice { get; set; }
+        [Required(ErrorMessage = "Please enter the amount of sleeves in stock")]
         public int StockAmount { get; set; }
+        // Active property to determine, whether to list a product
 
         public virtual ICollection<OrderLine> OrderLines { get; set; }
         public virtual ICollection<GameSleeve> GameSleeves { get; set; }
