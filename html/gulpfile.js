383e9c1705929008c4438108092792dde9b178ba
RoaldHaahr <roald_832@hotmail.com>
2016-12-06 22:19:23 +0100

    register.html added

--- html/gulpfile.js
+++ html/gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    notify = require('gulp-notify'),
+    livereload = require('gulp-livereload');
 
 var paths = {
     styles: 'assets/sass/source/**/*.scss',
@@ -9,10 +11,17 @@ var paths = {
 gulp.task('styles', function() {
     return gulp.src("assets/sass/source/main.scss")
         .pipe(sass())
+        .on('error', function(err) {
+        	notify().write(err);
+        	this.emit('end');
+        })
         .pipe(gulp.dest(paths.cssDest))
+        .pipe(livereload())
+        .pipe(notify({message: 'Sass compiled successfully!'}));
 });
 
 gulp.task('watch', function() {
+	livereload.listen();
     gulp.watch(paths.styles, ['styles']);
 });
 
