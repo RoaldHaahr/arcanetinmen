8db8644d380dc2a8abd46d88c127d25ed12fe60a
DESKTOP-DEUIBA9\edvin <edvinasurbasius@hotmail.com>
2016-12-12 14:44:47 +0100

    Edvinas commit. About + Contact Pages.

--- html/gulpfile.js
+++ html/gulpfile.js
@@ -1,23 +1,17 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    notify = require('gulp-notify'),
     livereload = require('gulp-livereload');
 
 var paths = {
     styles: 'assets/sass/source/**/*.scss',
-    cssDest: '../ArcaneTinmen/dist/css'
+    cssDest: 'dist/css'
 };
 
 gulp.task('styles', function() {
     return gulp.src("assets/sass/source/main.scss")
         .pipe(sass())
-        .on('error', function(err) {
-        	notify().write(err);
-        	this.emit('end');
-        })
         .pipe(gulp.dest(paths.cssDest))
-        .pipe(livereload())
-        .pipe(notify({message: 'Sass compiled successfully!'}));
+        .pipe(livereload());
 });
 
 gulp.task('watch', function() {
